d80d138557e2e1845bb4007619cc5c0d976753a5
visaladi <amdbadikari@gmail.com>
2025-10-14 22:36:32 +0530

    config changes

---
+++ configurations/config.py
@@ -0,0 +1,16 @@
+CHANNELS = {
+    "news": "1388537786650333305",
+    "general": "1398911682985594926",
+    "focus_based": "1343282779848183944",
+    "finder":"1388537786650333305"
+}
+headers = {
+        "accept-language": "en-US,en;q=0.9",
+        "authorization": "MTM0MzIzMzUzODM1ODc3NTgxOA.GWCvXq.tE9Q5n0RBR6qMLD_yTiRQ-Ogtw4FG9QBa59xPU",  # Replace securely
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+        "x-discord-locale": "en-US",
+        "cookie": "__dcfduid=49abb8400f0015eda5e6a635b2344b6a; __sdcfduid=494bb8400f0011eda5e6a635b2344b6a298b870d23dc4dc4dc2ed47448ff82791438eca9542b8ddd0aec833c93404851;"
+    }
+TWITTER_EMAIL = "aylan.harlen@doodrops.org"
+TWITTER_PASSWORD = "072.project"
+TWITTER_USERNAME = "@aylan7496042637"
